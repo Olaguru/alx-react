ec6e01fd8a6cfcf44cdc3c0d7e720cdcb5789667
olaguru <dapoodedeyi03@gmail.com>
2024-11-04 15:27:32 +0100

    task2

---
+++ task_2/dashboard/src/utils.js
@@ -0,0 +1,17 @@
+
+export function getFullYear(){
+    const date = new Date();
+    const year = date.getFullYear();
+    return year;
+}
+
+export function getFooterCopy(isIndex){
+        const trueState = "Holberton School";
+        const falseState = "Holberton School main dashboard";
+        if (isIndex) {
+        return (trueState);
+        }
+        else {
+        return (falseState)
+        }
+}
