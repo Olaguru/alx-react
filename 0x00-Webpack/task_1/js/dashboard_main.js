251f90f8471d17ce11a4384753c34ed5fe771c14
olaguru <dapoodedeyi03@gmail.com>
2024-11-05 09:38:08 +0100

    update

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,11 +1,13 @@
 import $ from 'jquery';
 import _ from 'lodash';
 
-$('body').append(<p>Holberton Dashboard</p>);
-$('body').append(<p>Dashboard data for the students</p>);
-$('body').append(<button>Click here to get started</button>);
-$('body').append(<p id='count'></p>);
-$('body').append(<p>Copyright - Holberton School</p>);
+let count = 0;
+
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append('<p id="count"></p>');
+$('body').append('<p>Copyright - Holberton School</p>');
 
 
 function updateCounter(){
