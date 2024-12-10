cf3ff5c95ae3563210f862f5902aa00afba829c1
olaguru <dapoodedeyi03@gmail.com>
2024-12-10 09:40:12 +0100

    enzyme and set up

---
+++ 0x01-react_intro/task_4/dashboard/src/App.js
@@ -0,0 +1,35 @@
+import logo from './logo.jpeg';
+import './App.css';
+import { getFooterCopy, getFullYear } from './utils';
+
+function App() {
+  return (
+    <div className='App'>
+    <header className='App-header'>
+      <img src={logo} alt='holberton-logo'></img>
+      <h1>School dashboard</h1>
+    </header>
+
+    <hr className='Horizontal-line'></hr>
+    <body className='App-body'>
+      <p>Login to access the full dashboard</p>
+      <form className='form-1'>
+        <label htmlfor='email'>Email:</label>
+        <input type='email' name='email' />
+        <label htmlfor='password'>Password:</label>
+        <input type='password' name='password' />
+        <button>OK</button>
+      </form>
+    </body>
+    
+    <hr className='Horizontal-line'></hr>
+    
+    <footer className='App-footer'>
+      <p>Copyright {getFullYear()} - {getFooterCopy()}</p>
+    </footer>
+    </div>
+   
+  );
+}
+
+export default App;
---
+++ 0x01-react_intro/task_4/dashboard/src/utils.test.js
@@ -0,0 +1,21 @@
+import { getFullYear } from "./utils";
+import { getFooterCopy } from "./utils";
+import { getLatestNotification } from "./utils";
+
+
+test('Test getFullYear', () => {
+    const currentYear = new Date().getFullYear();
+    expect(getFullYear()).toBe(currentYear);
+});
+
+test('Test getFooterCopy', () => {
+    expect(getFooterCopy(true)).toBe('ALX');
+})
+
+test('Test getFooterCopy', () => {
+    expect(getFooterCopy(false)).toBe('ALX main dashboard');
+})
+
+test('Test for getLatestNotification', () => {
+    expect(getLatestNotification()).toBe('<strong>Urgent requirement</strong> - complete by EOD');
+})
