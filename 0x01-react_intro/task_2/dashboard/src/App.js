801f18f3d2f96cd1829934962a3fcfffc4350a3d
olaguru <dapoodedeyi03@gmail.com>
2024-12-10 04:49:36 +0100

    task2 no 3

--- 0x01-react_intro/task_2/dashboard/src/App.js
+++ 0x01-react_intro/task_2/dashboard/src/App.js
@@ -13,6 +13,14 @@ function App() {
     <hr className='Horizontal-line'></hr>
     <body className='App-body'>
       <p>Login to access the full dashboard</p>
+      <form>
+        <label htmlfor='email'>Email:</label>
+        <input type='email' name='email' />
+        <label htmlfor='password'>Password:</label>
+        <input type='password' name='password' />
+        <button>OK</button>
+
+      </form>
     </body>
     
     <hr className='Horizontal-line'></hr>
