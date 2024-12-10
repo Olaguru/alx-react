93ffd4c69d681a8907c30805e0c44f79dd072e3e
olaguru <dapoodedeyi03@gmail.com>
2024-12-10 10:30:36 +0100

    test

--- 0x01-react_intro/task_3/dashboard/src/setupTests.js
+++ 0x01-react_intro/task_3/dashboard/src/setupTests.js
@@ -1,2 +1,5 @@
 // src/setupTests.js
-import '@testing-library/jest-dom/extend-expect';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
